f4f6e2fbc59ccd522463f7b7ad05be505b7ad3d5
kevin-zhang02 <71109540+kevin-zhang02@users.noreply.github.com>
2024-03-22 06:38:17 -0400

    Refactored code to allow row, col indexing into board.

--- MinesweeperAgent/minesweeper.py
+++ MinesweeperAgent/minesweeper.py
@@ -19,13 +19,85 @@ class Minesweeper:
         EIGHT = 8
         UNREVEALED = 9
 
-    def __init__(self, height: int, width: int, num_bombs: int):
-        self.height: int = height
-        self.width: int = width
-        self.num_bombs: int = num_bombs
+    class Board:
+        def __init__(self, height: int, width: int, num_bombs: int):
+            self.height: int = height
+            self.width: int = width
+            self.num_bombs: int = num_bombs
 
-        self._board: list[Minesweeper.State] \
-            = [Minesweeper.State.UNREVEALED] * (self.height * self.width)
+            self.board: list[Minesweeper.State] \
+                = [Minesweeper.State.UNREVEALED] * (self.height * self.width)
+
+            self._set_bombs()
+
+        @property
+        def cell_count(self) -> int:
+            return len(self.board)
+
+        def _set_bombs(self) -> None:
+            remaining_cells: int = self.height * self.width
+            remaining_bombs: int = self.num_bombs
+
+            cell: int
+            for cell in range(self.height * self.width):
+                rand_val: int = random.randint(0, remaining_cells - 1)
+                if rand_val < remaining_bombs:
+                    self.board[cell] = Minesweeper.State.BOMB
+                    remaining_bombs -= 1
+
+                    if not remaining_bombs:
+                        return
+
+                remaining_cells -= 1
+
+        def cell_to_rc(self, cell: int) -> tuple[int, int]:
+            """
+            :param cell: the state to convert into a (row, column) tuple.
+            :return: a tuple containing the row and column of the state.
+            """
+            return cell // self.width, cell % self.width
+
+        def cell_from_rc(self, r: int, c: int) -> int:
+            """
+            :param r: the row of the state.
+            :param c: the column of the state.
+            :return: the index of the state.
+            """
+            return r * self.width + c
+
+        def is_cell_inbounds(self, cell: int) -> bool:
+            return 0 <= cell < self.cell_count
+
+        def is_cell_inbounds_rc(self, r: int, c: int) -> bool:
+            return 0 <= r < self.height and 0 <= c < self.width
+
+        def count(self, state: "Minesweeper.State") -> int:
+            return self.board.count(state)
+
+        def _get_board_index(self, index: int | tuple[int, int]) -> int:
+            if isinstance(index, int):
+                return index
+            elif isinstance(index, tuple) and len(index) == 2:
+                try:
+                    return index[0] * self.width + index[1]
+                except TypeError:
+                    raise TypeError("Indices must be integers")
+            else:
+                raise TypeError(f"Expected int or tuple[int, int],"
+                                f" got {type(index)}")
+
+        def __getitem__(self,
+                        index: int | tuple[int, int]) -> "Minesweeper.State":
+            return self.board[self._get_board_index(index)]
+
+        def __setitem__(self,
+                        key: int | tuple[int, int],
+                        value: "Minesweeper.State") -> None:
+            self.board[self._get_board_index(key)] = value
+
+    def __init__(self, height: int, width: int, num_bombs: int):
+        self._board: Minesweeper.Board \
+            = Minesweeper.Board(height, width, num_bombs)
 
         self.first_reveal: bool = True
 
@@ -34,68 +106,56 @@ class Minesweeper:
         # 1: win
         self._game_state: int = 0
 
-        self._set_bombs()
+    @property
+    def height(self) -> int:
+        return self._board.height
 
-    def _cell_to_rc(self, cell: int) -> tuple[int, int]:
+    @property
+    def width(self) -> int:
+        return self._board.width
+
+    @property
+    def cell_count(self) -> int:
+        return self.height * self.width
+
+    def cell_to_rc(self, cell: int) -> tuple[int, int]:
         """
         :param cell: the state to convert into a (row, column) tuple.
-        :type cell: int
         :return: a tuple containing the row and column of the state.
-        :rtype: tuple[int, int]
         """
-        return cell // self.width, cell % self.width
+        return self._board.cell_to_rc(cell)
 
-    def _cell_from_rc(self, r: int, c: int) -> int:
+    def cell_from_rc(self, r: int, c: int) -> int:
         """
         :param r: the row of the state.
-        :type r: int
         :param c: the column of the state.
-        :type c: int
         :return: the index of the state.
-        :rtype: int
         """
-        return r * self.width + c
+        return self._board.cell_from_rc(r, c)
 
-    def _is_cell_inbounds(self, cell: int) -> bool:
-        return 0 <= cell < self.height * self.width
+    def is_cell_inbounds(self, cell: int) -> bool:
+        return 0 <= cell < self.cell_count
 
-    def _is_cell_inbounds_rc(self, r: int, c: int) -> bool:
+    def is_cell_inbounds_rc(self, r: int, c: int) -> bool:
         return 0 <= r < self.height and 0 <= c < self.width
 
-    def _set_bombs(self) -> None:
-        remaining_cells: int = self.height * self.width
-        remaining_bombs: int = self.num_bombs
-
-        cell: int
-        for cell in range(self.height * self.width):
-            rand_val: int = random.randint(0, remaining_cells - 1)
-            if rand_val < remaining_bombs:
-                self._board[cell] = Minesweeper.State.BOMB
-                remaining_bombs -= 1
-
-                if not remaining_bombs:
-                    return
-
-            remaining_cells -= 1
-
-    def _is_bomb(self, state: int) -> bool:
-        if not self._is_cell_inbounds(state):
+    def _is_bomb(self, row: int, col: int) -> bool:
+        if not self._board.is_cell_inbounds_rc(row, col):
             return False
         else:
-            return self._board[state] == Minesweeper.State.BOMB
+            return self._board[row, col] == Minesweeper.State.BOMB
 
     def _relocate_bomb(self, row: int, col: int) -> None:
-        assert self._is_bomb(self._cell_from_rc(row, col))
+        assert self._is_bomb(row, col)
 
         cell: int
         for cell in range(self.height * self.width):
             if self._board[cell] == Minesweeper.State.UNREVEALED:
                 self._board[cell] = Minesweeper.State.BOMB
-                self._board[self._cell_from_rc(row, col)] \
-                    = Minesweeper.State.UNREVEALED
+                self._board[row, col] = Minesweeper.State.UNREVEALED
                 return
 
-        raise BaseException
+        raise Exception("Can't relocate the bomb.")
 
     def _count_surrounding_bombs(self, row: int, col: int) -> int:
         surrounding_bombs: int = 0
@@ -104,10 +164,7 @@ class Minesweeper:
         for r in range(row - 1, row + 2):
             c: int
             for c in range(col - 1, col + 2):
-                surrounding_bombs \
-                    += 1 if (self._is_cell_inbounds_rc(r, c)
-                             and self._is_bomb(self._cell_from_rc(r, c))) \
-                    else 0
+                surrounding_bombs += 1 if self._is_bomb(r, c) else 0
 
         return surrounding_bombs
 
@@ -119,37 +176,36 @@ class Minesweeper:
                     row: int,
                     col: int
                     ) -> dict[tuple[int, int], "Minesweeper.State"] | None:
-        cell: int = self._cell_from_rc(row, col)
 
         if self.first_reveal:
             self.first_reveal = False
 
-            if self._board[cell] == Minesweeper.State.BOMB:
+            if self._board[row, col] == Minesweeper.State.BOMB:
                 self._relocate_bomb(row, col)
 
-        if self._is_bomb(cell):
+        if self._is_bomb(row, col):
             self._game_state = -1
             return None
-        elif self._board[cell] != Minesweeper.State.UNREVEALED:
+        elif self._board[row, col] != Minesweeper.State.UNREVEALED:
             return {}
         else:
-            self._board[cell] \
+            self._board[row, col] \
                 = Minesweeper.State(self._count_surrounding_bombs(row, col))
 
             if self._is_winner():
                 self._game_state = 1
                 return None
-            elif self._board[cell] != Minesweeper.State.ZERO:
-                return {(row, col): self._board[cell]}
+            elif self._board[row, col] != Minesweeper.State.ZERO:
+                return {(row, col): self._board[row, col]}
 
             updated: dict[tuple[int, int], Minesweeper.State] \
-                = {(row, col): self._board[cell]}
+                = {(row, col): self._board[row, col]}
 
             r: int
             for r in range(row - 1, row + 2):
                 c: int
                 for c in range(col - 1, col + 2):
-                    if self._is_cell_inbounds_rc(r, c):
+                    if self._board.is_cell_inbounds_rc(r, c):
                         rc_updated: dict[tuple[int, int], Minesweeper.State] \
                             = self.reveal_cell(r, c)
 
@@ -196,15 +252,10 @@ class Minesweeper:
         Gets a string representation of the object.
 
         :param horizontal_spacing: the amount of space given for each cell.
-        :type horizontal_spacing: int
         :param bomb_repr: gets the representation for the bomb.
-        :type bomb_repr: Callable[[int, int], str]
         :param unrevealed_repr: gets the representation for an unrevealed cell.
-        :type unrevealed_repr: Callable[[int, int], str]
         :param other_repr: gets the representation for other cells.
-        :type other_repr: Callable[[int, int, int], str]
         :return: a string representation of the object.
-        :rtype str:
         """
         output_format: str = "{:>" + str(horizontal_spacing) + "}"
 
@@ -232,7 +283,6 @@ class Minesweeper:
         Pretty print the state values.
 
         :return: string version of state values.
-        :rtype: str
         """
 
         return self.as_formatted_str()
